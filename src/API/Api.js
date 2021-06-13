8b0a083636c43a47ad42b0e9bf6dc1a8f70dea33
martino2197 <martino2197@gmail.com>
2021-06-13 16:40:45 -0500

    Airline Data in API

---
+++ src/API/Api.js
@@ -0,0 +1,20 @@
+export const Api = [
+    {"id":"1","name":"Aeroméxico","country":"Russia","active":"N","image":"https://logodownload.org/wp-content/uploads/2019/09/aeromexico-logo-3.png"}
+    ,
+    {"id":"2","name":"Interjet","country":"United Kingdom","active":"N","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Logo_Interjet_transparent.svg/800px-Logo_Interjet_transparent.svg.png"}
+    ,
+    {"id":"3","name":"Volaris","callsign":"MILE-AIR","country":"United States","active":"Y","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Volaris_logo.svg/768px-Volaris_logo.svg.png"}
+    ,
+    {"id":"4","name":"VivaAerobus","country":"United States","active":"N","image":"https://www.sebogo.mx/images/airlines/ori/VB.png"}
+    ,
+    {"id":"5","name":"EasyFly","country":"Russia","active":"N","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/EasyFly.svg/1024px-EasyFly.svg.png"}
+    ,
+    {"id":"6","name":"Avianca","country":"","active":"Y","image":"https://upload.wikimedia.org/wikipedia/commons/e/ec/Avianca_Logo_2013.png"}
+    ,
+    {"id":"7","name":"SATENA","country":"United States","active":"N","image":"https://seeklogo.com/images/S/satena-logo-6804455C47-seeklogo.com.png"}
+    ,
+    {"id":"8","name":"LATAM Colombia","country":"United Kingdom","active":"N","image":"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Latam-logo_-v_%28Indigo%29.svg/800px-Latam-logo_-v_%28Indigo%29.svg.png"}
+    ,
+    {"id":"9","name":"Wingo","country":"South Africa","active":"Y","image":"https://upload.wikimedia.org/wikipedia/commons/3/3b/Wingo_Logo.jpg"}
+]
+
